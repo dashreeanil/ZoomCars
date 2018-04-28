d1e2b2b751dca58e059aa5c44008d9936f6fabaa
Anil <dashreeanil@gmail.com>
2018-04-28 14:59:32 +0530

    Starting project

---
+++ Desktop/ZoomCars/project/src/main/java/UtiltyLIb/BaseClass.java
@@ -0,0 +1,105 @@
+package UtiltyLIb;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public abstract class BaseClass implements IAutoConst {
+	
+	 ExtentReports extent;
+	 ExtentTest logger;
+	public WebDriver driver;
+	 
+	 
+	 @BeforeTest
+	 public void startReport(){
+	 //ExtentReports(String filePath,Boolean replaceExisting) 
+	 //filepath - path of the file, in .htm or .html format - path where your report needs to generate. 
+	 //replaceExisting - Setting to overwrite (TRUE) the existing file or append to it
+	 //True (default): the file will be replaced with brand new markup, and all existing data will be lost. Use this option to create a brand new report
+	 //False: existing data will remain, new tests will be appended to the existing report. If the the supplied path does not exist, a new file will be created.
+	 extent = new ExtentReports (System.getProperty("user.dir") +"/test-output/STMExtentReport.html", true);
+	 //extent.addSystemInfo("Environment","Environment Name")
+	 extent
+	                .addSystemInfo("Host Name", "SoftwareTestingMaterial")
+	                .addSystemInfo("Environment", "Automation Testing")
+	                .addSystemInfo("User Name", "Rajkumar SM");
+	                //loading the external xml file (i.e., extent-config.xml) which was placed under the base directory
+	                //You could find the xml file below. Create xml file in your project and copy past the code mentioned below
+	                extent.loadConfig(new File(System.getProperty("user.dir")+"\\extent-config.xml"));
+	 }
+	 
+	 @BeforeMethod
+	 public void openBrowser()
+	 {
+		 System.setProperty(CHROME_KEY, CHROME_VALUE);
+		 driver = new ChromeDriver();
+		 driver.get(URL);
+		 driver.manage().timeouts().implicitlyWait(ITO, TimeUnit.SECONDS);
+		 driver.manage().window().maximize();
+		 
+	 }
+	 
+//	 @Test
+//	 public void passTest(){
+//	 //extent.startTest("TestCaseName", "Description")
+//	 //TestCaseName – Name of the test
+//	 //Description – Description of the test
+//	 //Starting test
+//	 logger = extent.startTest("passTest");
+//	 Assert.assertTrue(true);
+//	 //To generate the log when the test case is passed
+//	 logger.log(LogStatus.PASS, "Test Case Passed is passTest");
+//	 }
+//	 
+//	 @Test
+//	 public void failTest(){
+//	 logger = extent.startTest("failTest");
+//	 Assert.assertTrue(false);
+//	 logger.log(LogStatus.PASS, "Test Case (failTest) Status is passed");
+//	 }
+//	 
+//	 @Test
+//	 public void skipTest(){
+//	 logger = extent.startTest("skipTest");
+//	 throw new SkipException("Skipping - This is not ready for testing ");
+//	 }
+	 
+	 @AfterMethod
+	 public void getResult(ITestResult result){
+	 if(result.getStatus() == ITestResult.FAILURE){
+	 logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getName());
+	 logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getThrowable());
+	 }else if(result.getStatus() == ITestResult.SKIP){
+	 logger.log(LogStatus.SKIP, "Test Case Skipped is "+result.getName());
+	 }
+	 // ending test
+	 //endTest(logger) : It ends the current test and prepares to create HTML report
+	 extent.endTest(logger);
+	 driver.quit();
+	 }
+	 @AfterTest
+	 public void endReport(){
+	 // writing everything to document
+	 //flush() - to write or update test information to your report. 
+	                extent.flush();
+	                //Call close() at the very end of your session to clear all resources. 
+	                //If any of your test ended abruptly causing any side-affects (not all logs sent to ExtentReports, information missing), this method will ensure that the test is still appended to the report with a warning message.
+	                //You should call close() only once, at the very end (in @AfterSuite for example) as it closes the underlying stream. 
+	                //Once this method is called, calling any Extent method will throw an error.
+	                //close() - To close all the operation
+	                extent.close();
+	    }
+
+
+}
---
+++ Desktop/ZoomCars/project/src/main/java/UtiltyLIb/IAutoConst.java
@@ -0,0 +1,15 @@
+package UtiltyLIb;
+
+public interface IAutoConst {
+	
+	String CHROME_KEY ="webdriver.chrome.driver";
+	String CHROME_VALUE="./resources/chromedriver.exe";
+	String FIREFOX_KEY ="webdriver.gecko.driver";
+	String FIREFOX_VALUE="./resources/geckodriver.exe";
+	String URL ="https://www.zoomcar.com";
+	int ETO=20;
+	int ITO=20;
+	
+	
+
+}
